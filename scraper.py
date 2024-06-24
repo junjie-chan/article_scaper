b974031e1de9072b4f360694433d6c8e1745265a
Junjie Chen <junjie.chan@outlook.com>
2024-06-24 15:50:49 +1000

    Scrape "Recent Articles" section.

--- scraper.py
+++ scraper.py
@@ -13,6 +13,13 @@ service = Service(chrome_driver_path)
 driver = webdriver.Chrome()
 driver.get('https://southeastasiainfra.com/category/urban-infrastructure/')
 
+li_tags = driver.find_elements(By.XPATH, '//ul[@class="widget_list"]/li')
+for li in li_tags:
+    link = li.find_element(By.XPATH, './div/a').get_attribute('href')
+    title = li.find_element(By.XPATH, './div/a').text
+    time = li.find_element(By.XPATH, './div/div/span[@class="updated"]').text
+    country = li.find_element(By.XPATH, './div/div/a[last()]').text
+    print(f'title: {title}\nlink: {link}\ntime: {time}\ncountry: {country}\n\n')
 
 input('Press enter to quit...')
 driver.quit()
